3fec2f9c727ab72c17b39d113f9b7a99786d53bf
Zhenya Yakovenko <zhenyayakovenko07@gmail.com>
2025-12-31 13:14:10 -0600

    Changing status codes for get and create post.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -37,15 +37,23 @@ def get_posts():
     return {"data": my_posts}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.model_dump()
     post_dict["id"] = randrange(0, 1000000)
-    print(post_dict)
     my_posts.append(post_dict)
     return {"data": post_dict}
 
 
 @app.get("/posts/{id}")
-def get_post(id: int):
-    return {"post_detail": find_post(id)}
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": f"Post with id: {id} was not found."}
+        # OR USE HTTPEXCEPTION
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id: {id} was not found.",
+        )
+    return {"post_detail": post}
