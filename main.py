9d594d0e062e92eddd37d7c5fa84e973b663671f
Zhenya Yakovenko <zhenyayakovenko07@gmail.com>
2025-12-31 13:05:42 -0600

    Create post and get post by id.

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
+
+# uvicorn main:app --reload
 
 
 class Post(BaseModel):
@@ -13,6 +16,17 @@ class Post(BaseModel):
     rating: Optional[int] = None  # Optional field
 
 
+my_posts = [
+    {"title": "The title", "content": "Some interesting content of this post", "id": 1}
+]
+
+
+def find_post(id: int) -> dict:
+    for post in my_posts:
+        if post["id"] and post["id"] == id:
+            return post
+
+
 @app.get("/")
 def root():
     return {"message": "Welcome to my api"}
@@ -20,13 +34,18 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(post: Post):
-    print(post)  # PyDentic instance
-    print(post.dict())  # Convert to python dict
-    print(post.title)
-    print(post.published, post.rating)
-    return {"data": "new post created"}
+    post_dict = post.model_dump()
+    post_dict["id"] = randrange(0, 1000000)
+    print(post_dict)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    return {"post_detail": find_post(id)}
