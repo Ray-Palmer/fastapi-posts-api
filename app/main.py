3b18d2012229672f5d96254edde7548176a66bcc
Zhenya Yakovenko <zhenyayakovenko07@gmail.com>
2026-01-14 13:07:24 -0600

    Refactor and clean up the code

--- app/main.py
+++ app/main.py
@@ -1,121 +1,20 @@
-import psycopg
-import time
+from fastapi import FastAPI
 
-import app.my_config as my_config
-from app import models, schemas, utils
-from app.database import engine, get_db
+from app import models
+from app.database import engine
+from app.routers import post, user
 
-from fastapi import FastAPI, status, HTTPException, Response, Depends
-from psycopg.rows import dict_row
-from sqlalchemy.orm import Session
-from typing import List
+# uvicorn app.main:app --reload
+# http://127.0.0.1:8000/docs
 
 models.Base.metadata.create_all(bind=engine)  # Create models
 
 app = FastAPI()
 
-# uvicorn app.main:app --reload
-# http://127.0.0.1:8000/docs
-
-
-while True:
-    try:
-        conn = psycopg.connect(my_config.DB_INFO, row_factory=dict_row)
-        cursor = conn.cursor()
-        print("Database connection was succesfull!")
-        break
-    except Exception as error:
-        print("Connecting to database failed.")
-        print(error)
-        time.sleep(2)
+app.include_router(post.router)  # include post routes
+app.include_router(user.router)  # include user routes
 
 
 @app.get("/")
 def root():
     return {"message": "Welcome to my api"}
-
-
-@app.get("/posts", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return posts
-
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
-    new_post = models.Post(**post.model_dump())
-    db.add(new_post)
-    db.commit()  # save new post to db
-    db.refresh(new_post)
-    return new_post
-
-
-@app.get("/posts/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-
-    if post is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with id: {id} was not found.",
-        )
-
-    return post
-
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == id)
-
-    if post.first() is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with id: {id} does not exist.",
-        )
-
-    post.delete(synchronize_session=False)
-    db.commit()
-
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.put("/posts/{id}", response_model=schemas.Post)
-def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-
-    if post_query.first() is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with id: {id} does not exist.",
-        )
-
-    post_query.update(post.model_dump(), synchronize_session=False)
-    db.commit()
-    updated_post = post_query.first()
-
-    return updated_post
-
-
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    user.password = utils.hash(user.password)  # hash the password
-
-    new_user = models.User(**user.model_dump())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
-
-
-@app.get("/users/{id}", response_model=schemas.UserOut)
-def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-
-    if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with id: {id} does not exist.",
-        )
-
-    return user
