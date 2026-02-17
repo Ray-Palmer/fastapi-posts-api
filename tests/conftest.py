4a9c5b91ac0903b5c3abf6700bd86d680bd51067
Zhenya Yakovenko <zhenyayakovenko07@gmail.com>
2026-02-17 14:29:25 -0600

    Tests for user's routes.

---
+++ tests/conftest.py
@@ -0,0 +1,56 @@
+"""
+Provides fixtures for all test-funcs in the same directory.
+We don't need to import 'session' or 'client' to get access to them.
+All is done by pytest automatically.
+"""
+
+import pytest
+
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.main import app
+from app.config import settings
+from app.database import get_db, Base
+
+# Create a connection to the testing database
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture
+def session():
+    Base.metadata.drop_all(bind=engine)  # drop tables
+    Base.metadata.create_all(bind=engine)  # create tables
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+
+    app.dependency_overrides[get_db] = override_get_db  # Swap dev db to the testing db
+
+    yield TestClient(app)  # start test
+
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "spongebob@gmail.com", "password": "password123"}
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user["password"] = user_data["password"]
+    return new_user
